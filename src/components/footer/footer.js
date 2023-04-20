52b7fbd855c4add067ce119296ce100ccb5c3c1f
nashhabibon <nashrudinhabibon@gmail.com>
2023-04-20 20:20:35 +0800

    Initial commit for footer section

---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.css";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <footer>
+        <h3>© Copyright 2023</h3>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;

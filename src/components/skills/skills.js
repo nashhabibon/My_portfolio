161c4524207faa72f8edacca24d317dfd58bd167
nashhabibon <nashrudinhabibon@gmail.com>
2023-05-06 12:57:51 +0800

    made some changes of design

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Graphic from "./graphic";
 import Frontend from "./frontend";
 import Backend from "./backend";
-// import Swiperskills from "./swiperskills";
 import "./skills.css";
 
 const Skills = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const tabs = [
+    { label: "Graphic Designer", content: <Graphic /> },
+    { label: "Frontend Developer", content: <Frontend /> },
+    { label: "Backend Developer", content: <Backend /> },
+  ];
+
+  function handleTabClick(index) {
+    setActiveTabIndex(index);
+  }
   return (
     <section className="skills section" id="skills">
       <div className="container_title">
@@ -13,10 +22,24 @@ const Skills = () => {
         <span className="section_subtitle"> My Technical Level</span>
       </div>
       <div className="skills_container container">
-        {/* <Swiperskills /> */}
-        <Graphic />
-        <Frontend />
-        <Backend />
+        <div className="tab_container">
+          <div className="vertical_tabs">
+            <ul>
+              {tabs.map((tab, index) => (
+                <li
+                  key={index}
+                  className={index === activeTabIndex ? "active" : ""}
+                  onClick={() => handleTabClick(index)}
+                >
+                  {tab.label}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="tab_content">
+            <div className="skills_box">{tabs[activeTabIndex].content}</div>
+          </div>
+        </div>
       </div>
     </section>
   );

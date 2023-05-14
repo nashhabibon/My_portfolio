4e56da96f366646c58d8ed72b4407acb9efebb8a
nashhabibon <nashrudinhabibon@gmail.com>
2023-05-14 12:45:24 +0800

    add function on resume button

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./home.css";
 import Image from "../../assets/pic.png";
 import { Fade, Slide } from "react-awesome-reveal";
+import Resume from "../../../src/assets/Habibon_Nashrudin_Resume.docx";
+
 const home = () => {
+  const downloadDocx = () => {
+    const url = Resume;
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "Habibon_Resume.docx");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  console.log(downloadDocx);
   return (
     <section className="home section" id="home">
       <div className="container home_container">
@@ -19,7 +31,9 @@ const home = () => {
               </h3>
             </Fade>
           </div>
-          <button className="cv_btn">Resume</button>
+          <button className="cv_btn" onClick={downloadDocx}>
+            Resume
+          </button>
         </div>
 
         <div className="home_container_right">

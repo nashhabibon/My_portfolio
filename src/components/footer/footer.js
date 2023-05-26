604e74ceab6e660d7ec1ec9ae9b0a12b0310b5cb
nashhabibon <nashrudinhabibon@gmail.com>
2023-05-26 21:42:21 +0800

    add social media button in footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,14 +1,60 @@
 import React from "react";
 import "./footer.css";
-import { AiOutlineCopyrightCircle } from "react-icons/ai";
+import { AiOutlineBehance } from "react-icons/ai";
+
+import { FiGithub } from "react-icons/fi";
+import { SiUpwork } from "react-icons/si";
+import { GrLinkedinOption, GrFacebookOption } from "react-icons/gr";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <footer>
-        <h3>© Copyright 2023</h3>
-      </footer>
-    </div>
+    <section className="footer section">
+      <div className="footer-container container">
+        <span>Design & Built by Nashrudin Habibon</span>
+
+        <div className="social_media">
+          <a
+            href="https://github.com/nashhabibon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiGithub />
+          </a>
+
+          <a
+            href="https://www.behance.net/nashrudin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineBehance />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/nashrudin-habibon-433ab8a8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrLinkedinOption />
+          </a>
+
+          <a
+            href="https://www.facebook.com/NashrudinHabibon/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GrFacebookOption />
+          </a>
+
+          <a
+            href="https://www.upwork.com/freelancers/nashrudinh"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <SiUpwork />
+          </a>
+        </div>
+      </div>
+    </section>
   );
 };
 
